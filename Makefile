9f8094c6b04f98f48779a3d7e143630c9afd5ad3
sosomasox <sosomasox@gmail.com>
2026-01-30 18:46:45 +0900

    update

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: docker-build-and-push
 docker-build-and-push:
-	sudo docker build --no-cache -t vsrecorder/decktype-web:latest . && sudo docker push vsrecorder/decktype-web:latest
+	docker build --no-cache -t vsrecorder/decktype-web:latest . && docker push vsrecorder/decktype-web:latest
+
+.PHONY: deploy
+deploy:
+	docker compose pull && docker compose down && docker compose up -d
--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = "https://vsrecorder.mobi/api/v1/decktypes";
 
 export async function fetchDeckByCode(environmentId: string, deckcode: string): Promise<DeckType[]> {
   try {
-    const url = `${API_BASE_URL}/${deckcode}/${environmentId}`
+    const url = `${API_BASE_URL}/${deckcode}/environments/${environmentId}`
     const response = await fetch(url, {
       cache: "no-store",
     });
